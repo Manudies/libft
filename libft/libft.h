7952a05a51072ef118b95520a14e29bfe2d97fba
Manudies <manudies@gmail.com>
2025-04-29 14:09:11 +0200

    feat: bzero

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdiestre <mdiestre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:24:00 by mdiestre          #+#    #+#             */
-/*   Updated: 2025/04/29 11:48:29 by mdiestre         ###   ########.fr       */
+/*   Updated: 2025/04/29 13:19:21 by mdiestre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,4 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void *ft_memset(void *b, int c, size_t len);
+void ft_bzero(void *s, size_t n);
